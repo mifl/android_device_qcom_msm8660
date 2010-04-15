c4baa83aff24e9f53f3b8f37f7585d0e33a269ee
Raj Kushwaha <c_rajk@quicinc.com>
2010-04-15 11:52:32 -0700

    Initial Board Config Files for msm8660_surf

---
+++ BoardConfig.mk
@@ -0,0 +1,66 @@
+# Copyright (c) 2010, Code Aurora Forum.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+ifeq ($(QC_PROP),true)
+    BOARD_USES_QCOM_HARDWARE := true
+    BOARD_USES_ADRENO_200 := true
+    HAVE_ADRENO200_SOURCE := true
+    HAVE_ADRENO200_SC_SOURCE := true
+    HAVE_ADRENO200_FIRMWARE := true
+    BOARD_USES_QCOM_AUDIO_V2 := true
+    BOARD_USE_QCOM_TESTONLY := true
+
+    ifneq ($(BUILD_TINY_ANDROID), true)
+    BOARD_GPS_LIBRARIES := libloc
+    BOARD_CAMERA_LIBRARIES := libcamera
+    BOARD_HAVE_BLUETOOTH := false
+    BOARD_HAS_QCOM_WLAN := false
+    BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+    endif   # !BUILD_TINY_ANDROID
+
+else
+    BOARD_USES_GENERIC_AUDIO := true
+    USE_CAMERA_STUB := true
+
+endif # QC_PROP
+
+TARGET_HAVE_TSLIB := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
+TARGET_NO_RADIOIMAGE := true
+
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_CPU_ABI := armeabi
+TARGET_ARCH_VARIANT := armv7-a
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+TARGET_HARDWARE_3D := false
+TARGET_BOARD_PLATFORM := msm8k
+TARGET_BOOTLOADER_BOARD_NAME := 8x60
+
+BOARD_KERNEL_BASE    := 0x00200000
+BOARD_NAND_PAGE_SIZE := 4096
+
+TARGET_USERIMAGES_USE_EXT2 := true
+
+BOARD_KERNEL_CMDLINE := console=ttyDCC0 androidboot.hardware=qcom
+BOARD_EGL_CFG := vendor/qcom/$(TARGET_PRODUCT)/egl.cfg
---
+++ msm8660_surf.mk
@@ -0,0 +1,16 @@
+PRODUCT_PACKAGES := \
+    IM \
+    VoiceDialer \
+    SdkSetup \
+    FM
+
+
+$(call inherit-product, build/target/product/generic.mk)
+
+#Enabling Ring Tones
+include frameworks/base/data/sounds/OriginalAudio.mk
+
+# Overrides
+PRODUCT_BRAND := qcom
+PRODUCT_NAME := msm8660_surf
+PRODUCT_DEVICE := msm8660_surf
