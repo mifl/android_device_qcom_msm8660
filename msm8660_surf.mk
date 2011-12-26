cdc6ea8ecad306a27ede80d462db467dd7edfb68
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2011-12-26 17:43:52 +0530

    msm8660_surf: Add new wallpapers available in ICS
    Change-Id: Id78fc3598cf3d37da688c7136f9beef7dc7f3c11
    CRs-Fixed: 324530

--- msm8660_surf.mk
+++ msm8660_surf.mk
@@ -2,3 +2,10 @@ $(call inherit-product, device/qcom/common/common.mk)
 
 PRODUCT_NAME := msm8660_surf
 PRODUCT_DEVICE := msm8660_surf
+
+# Wallpapers
+PRODUCT_PACKAGES += Galaxy4
+PRODUCT_PACKAGES += HoloSpiralWallpaper
+PRODUCT_PACKAGES += MagicSmokeWallpapers
+PRODUCT_PACKAGES += NoiseField
+PRODUCT_PACKAGES += PhaseBeam
